9157098a98783241e64ecc3f380179994fae28f0
dantmam <danielssj85@gmail.com>
2023-11-26 18:12:23 -0300

    Menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Menu from "./pages/menu/menu";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ListarTarefa />
+      <Menu />
     </div>
   );
 }
